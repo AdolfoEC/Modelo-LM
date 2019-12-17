b4c1a6f6599ed1229bd1488340ef8dc7126f89bd
AdolfoEC <adolfoecc@gmail.com>
2019-12-17 09:34:01 -0600

    Se agrega variable al modelo y gráfica

--- lm_20191217.r
+++ lm_20191217.r
@@ -9,8 +9,19 @@ set.seed(123) ## Fijar semilla
 
 x <- sort( rnorm(100) )
 y <- sort( rexp(100,3) )
+z <- sort( rnorm(100) , decreasing = T)
 
 ## Regresión lineal
 
 modelo0 <- lm(x ~ y)
 summary(modelo0)
+
+modelo1 <- lm(x ~ y + z)
+summary(modelo1)
+
+## Gráfica ajuste
+
+plot( modelo1$fitted.values, x)
+abline(0,1)
+
+
